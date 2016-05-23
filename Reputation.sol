f44fe01e05d3398e72d83eee7bdcc46ebd326fed
Samuel Colbran <contact@samuco.net>
2016-05-23 23:02:00 +1000

    extended reputation

--- Reputation.sol
+++ Reputation.sol
@@ -12,7 +12,9 @@ contract Reputation {
 	struct Ticket {
 		address provider;
 		address consumer;
-		bool signed; // A flag to see whether the consumer has signed this ticket
+		bool signed; 		// A flag to see whether the consumer has signed this ticket
+		int8 rating;		// Optional to represent satisfaction (eg +1 is positive, -1 is negative). Meaning is dependant on service. 
+		string description; // Optional to describe the reason that this rating was given. 
 	}
 	
 	// Creates a new unsigned ticket
@@ -20,16 +22,16 @@ contract Reputation {
 		//if (msg.value < 1) throw; // Tickets cost 1 ether [to pay for future gas costs]
 		
 		// Create the ticket
-		services[msg.sender][tid] = Ticket(provider, consumer, false);
+		services[msg.sender][tid] = Ticket(provider, consumer, false, 0, "");
 	}
 	
 	// Signs a ticket
-	function signTicket(address service, uint256 token) {
+	function signTicket(address service, uint256 token, int8 rating, string description) {
 		Ticket ticket = services[service][token];
 		if (ticket.consumer != msg.sender) throw; // The consumer must sign the ticket
 		if (ticket.signed) throw;
 		ticket.signed = true;
-
-		// Refund the consumer
+		ticket.rating = rating;
+		ticket.description = description;
 	}	
 }
